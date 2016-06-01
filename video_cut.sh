2cdad46e23812d7d1d22536b1cf2b46e1fe62287
4815162342lost <family-gan@yandex.ru>
2016-06-01 22:00:14 +0400

    Create video_cut.sh

---
+++ video_cut.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+read -p "Enter file name: " file_name
+read -p "Enter start time (format hh:mm:ss): " start_time
+read -p "Enter file duration (format hh:mm:ss): " end_time
+read -p "Enter output file name (press enter for default output name: " output_name
+if [ "$output_name" = "" ]
+	then
+	echo $output_name
+	output_name="cut"$file_name
+	fi
+echo $output_name
+ffmpeg -i $file_name -ss $start_time -t $end_time -vcodec copy -acodec copy $output_name
